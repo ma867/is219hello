476a6e346360c5e9c3d2ce4c323555d5e510c40d
ma867 <ma867@njit.edu>
2020-03-09 15:44:00 -0400

    city path

--- test/CvsReader.test.js
+++ test/CvsReader.test.js
@@ -6,3 +6,10 @@ test('read cvs parse it and print it out', () => {
     expect(csvreader.readCSVAndParse(csvFile)).toStrictEqual(output);
 });
 
+test('read cvs parse it and print it out', () => {
+    let csvFile = "data/WorldCitiesShortList.csv";
+    let path = csvFile.split('.');
+    let extension = path[1];
+    expect(extension).toBe('csv');
+});
+
--- test/city.test.js
+++ test/city.test.js
@@ -9,3 +9,4 @@ test('Can create a city object', () => {
     let city = City.createCity(['São Paulo','Sao Paulo',-23.5587,-46.625,'Brazil','BR','BRA','São Paulo','admin',18845000,1076532519]);
     expect(city).toBeInstanceOf(City);
 });
+
