16729dacf10c1a8e20a8637d9288d4e77cd8925f
ma867 <ma867@njit.edu>
2020-03-09 15:28:39 -0400

    city path

--- test/CvsReader.test.js
+++ test/CvsReader.test.js
@@ -1,7 +1,7 @@
 const csvreader = require('../CsvReader');
 
 test('read cvs parse it and print it out', () => {
-    let csvFile = "C:\\Users\\MAlzate\\WebstormProjects\\is219hello\\data\\WorldCitiesShortList.csv";
+    let csvFile = "data/WorldCitiesShortList.csv";
     let output = csvreader.readCSVAndParse(csvFile);
     expect(csvreader.readCSVAndParse(csvFile)).toStrictEqual(output);
 });
